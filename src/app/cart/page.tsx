ee28e3d81ebdea37ba4b9a96f265f47758cd4636
umergulkaleem <umergulkaleem>
2024-12-15 15:40:47 +0500

    contact and card page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -43,7 +43,73 @@ const page = () => {
           </div>
         </div>
       </div>
-      <div>middle div</div>
+      <div>
+        <div className="w-[1440px] ml-[-20px] h-[480px] flex justify-between p-8">
+          <div className="w-2/3">
+            <div className="flex justify-between p-4 mt-[35px]">
+              <span className="ml-[200px] font-medium text-[16px] leadong-[24px]">
+                Product
+              </span>
+              <span className="ml-[100px] font-medium text-[16px] leadong-[24px]">
+                Price
+              </span>
+              <span className=" ml-[40px] font-medium text-[16px] leadong-[24px]">
+                Quantity
+              </span>
+              <span className=" mr-[130px] font-medium text-[16px] leadong-[24px]">
+                Subtotal
+              </span>
+            </div>
+            <div className="flex justify-between mt-[30px] ml-[50px] items-center p-4 border-b">
+              <div className="flex items-center">
+                <Image
+                  src="Asgaard sofa 1.svg"
+                  alt="Asgaard sofa"
+                  width={112.99}
+                  height={92.02}
+                />
+                <span className="ml-[30px] text-[16-px] font-normal leading-[24px] text-[#9F9F9F]">
+                  Asgaard sofa
+                </span>
+              </div>
+              <span className="ml-[30px] text-[16-px] font-normal leading-[24px] text-[#9F9F9F]">
+                Rs. 250,000.00
+              </span>
+              <p className="w-[32px] h-[32px] border-[1px] border-[#9F9F9F] text-justify ">
+                <p className="ml-[12px] mt-[4px]"> 1</p>
+              </p>
+              <span className="font-normal text-[16px] leading-[24px]">
+                Rs. 250,000.00
+              </span>
+              <Image src="trash.svg" width={28} height={28} alt="garbage can" />
+            </div>
+          </div>
+          <div className="w-1/3 p-8 ml-[120px]">
+            <h2 className="text-[32px] font-semibold leading-[48px] ml-[50px]   mb-4">
+              Cart Totals
+            </h2>
+            <div className="flex justify-between mb-2">
+              <span className="font-medium text-[16px] mt-[50px] mb-[30px] leading-[24px]">
+                Subtotal
+              </span>{" "}
+              <span className="font-normal text-[#9F9F9F] text-[16px] mt-[50px] mr-[90px] leading-[24px]">
+                Rs. 250,000.00
+              </span>
+            </div>
+            <div className="flex justify-between mb-4">
+              <span className="font-medium text-[16px] leading-[24px]">
+                Total
+              </span>
+              <span className=" text-[#B88E2F] font-normal text-[20px] mr-[90px] leading-[30px]">
+                Rs. 250,000.00
+              </span>
+            </div>
+            <button className=" border-black w-[222px] h-[58.95px] border-[1px] mt-[20px] ml-[10px] rounded-[15px]">
+              Check Out
+            </button>
+          </div>
+        </div>
+      </div>
       <div>
         <div className="flex justify-around w-full bg-gray-100 p-8 mt-[100px] mb-[280px] gap-[50px]">
           <div className="text-center">
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -43,9 +43,131 @@ const page = () => {
           </div>
         </div>
       </div>
-      <div>middle div</div>
       <div>
-        <div className="flex justify-around w-full bg-gray-100 p-8 mt-[100px] mb-[280px] gap-[50px]">
+        <div className="w-[1440px]  mt-[100px] h-[1144px]">
+          <div className="bg-white w-full">
+            <div className="text-[36px] font-semibold mb-4 leading-[54px] text-center">
+              Get In Touch With Us
+            </div>
+            <p className="text-center font-normal text-[16px] ml-[25%] w-[650px] leading-[24px] text-[#9F9F9F] mb-8">
+              For More Information About Our Product & Services. Please Feel
+              Free To Drop Us An Email. Our Staff Always Be There To Help You
+              Out. Do Not Hesitate!
+            </p>
+            <div className="flex mt-[150px] gap-[200px] justify-center">
+              <div>
+                <div className="mb-8">
+                  <span className="inline-flex items-center">
+                    {" "}
+                    <Image
+                      src="map-pin.svg"
+                      width={22}
+                      height={28.18}
+                      alt="location icon"
+                    />{" "}
+                  </span>{" "}
+                  <span className="text-[24px] font-medium  leading-[36px] inline-block ml-2">
+                    Address
+                  </span>
+                  <p className="font-normal ml-[35px] text-[16px] leading-[24px] ">
+                    236 5th SE Avenue, New{" "}
+                    <p className="font-normal text-[16px] leading-[24px] ">
+                      {" "}
+                      York NY10000, United
+                    </p>
+                    <p className="font-normal text-[16px] leading-[24px] ">
+                      {" "}
+                      States
+                    </p>
+                  </p>
+                </div>
+                <div className="mb-8">
+                  <span className="inline-flex items-center">
+                    {" "}
+                    <Image
+                      src="phone.svg"
+                      width={22}
+                      height={28.18}
+                      alt="location icon"
+                    />{" "}
+                  </span>{" "}
+                  <span className="text-[24px] font-medium  leading-[36px] inline-block ml-2">
+                    Phone
+                  </span>
+                  <p className="font-normal ml-[35px]  text-[16px] leading-[24px] ">
+                    Mobile: (+84) 546-6789
+                  </p>{" "}
+                  <p className="font-normal ml-[35px] text-[16px] leading-[24px] ">
+                    Hotline: (+84) 456-6789
+                  </p>
+                </div>
+                <div className="mb-8">
+                  <span className="inline-flex items-center">
+                    {" "}
+                    <Image
+                      src="clock-1.svg"
+                      width={22}
+                      height={28.18}
+                      alt="location icon"
+                    />{" "}
+                  </span>{" "}
+                  <span className="text-[24px] font-medium  leading-[36px] inline-block ml-2">
+                    Working Time
+                  </span>
+                  <p className="font-normal ml-[35px] text-[16px] leading-[24px] ">
+                    Monday-Friday: 9:00 - 22:00
+                  </p>
+                  <p className="font-normal ml-[35px] text-[16px] leading-[24px] ">
+                    Saturday-Sunday: 9:00 - 21:00
+                  </p>
+                </div>
+              </div>
+              <form>
+                <div className="mb-4">
+                  <div className="font-medium text-[16px] leading-[24px] ">
+                    Your name
+                  </div>
+                  <div className="w-[528.75px]  h-[75px] text-[#9F9F9F] border-[#9F9F9F] border-[1px] p-2  rounded-[10px] mt-[20px] mb-[30px]">
+                    <p className="mt-[16px] ml-[15px]">Alice</p>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <div className="font-medium text-[16px] leading-[24px] ">
+                    Email address
+                  </div>
+                  <div className="w-[528.75px] h-[75px] text-[#9F9F9F] border-[#9F9F9F] border-[1px] p-2  rounded-[10px] mt-[20px] mb-[30px]">
+                    <p className="mt-[16px] ml-[15px]"> alice@example.com</p>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <div className="font-medium text-[16px] leading-[24px] ">
+                    Subject
+                  </div>
+                  <div className="w-[528.75px] h-[75px] text-[#9F9F9F] border-[#9F9F9F] border-[1px] p-2  rounded-[10px] mt-[20px] mb-[30px]">
+                    <p className="mt-[16px] ml-[15px]">This is optional </p>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <div className="font-medium text-[16px] leading-[24px] ">
+                    Message
+                  </div>
+                  <div className="w-[528.75px] h-[75px] text-[#9F9F9F] border-[#9F9F9F] border-[1px] p-2  rounded-[10px] mt-[20px] mb-[30px]">
+                    <p className="mt-[16px] ml-[15px]">
+                      {" "}
+                      I'd like to talk about...{" "}
+                    </p>
+                  </div>
+                </div>
+                <div className="w-[237px] mt-[50px] mr-[10px] h-[48px] border-[1px] border-black  p-2 rounded-[15px]">
+                  <p className="ml-[80px] mt-[5px]"> Submit </p>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="flex justify-around w-full bg-gray-100 p-8  mb-[280px] gap-[50px]">
           <div className="text-center">
             <h2 className="font-medium text-[40px]">Free Delivery</h2>
             <p className="font-light text-[#9f9f9f] w-[250px]">
